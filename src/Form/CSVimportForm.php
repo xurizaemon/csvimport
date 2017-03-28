3ad5c0d3170933807ca5658476288cec3aaa61f3
Norman Kämper-Leymann <norman.leymann@gmail.com>
2017-03-28 17:33:19 +0200

    Add form

---
+++ src/Form/CSVimportForm.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\csvimport\Form\CSVimportForm.
+ */
+
+namespace Drupal\csvimport\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements the ajax demo form controller.
+ *
+ * This example demonstrates using ajax callbacks to populate the options of a
+ * color select element dynamically based on the value selected in another
+ * select element in the form.
+ *
+ * @see \Drupal\Core\Form\FormBase
+ * @see \Drupal\Core\Form\ConfigFormBase
+ */
+class CSVimportForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'csvimport_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['submit'] = [
+      '#type'  => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Final submit
+  }
+}
