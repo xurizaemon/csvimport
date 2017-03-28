45ec460976a38c75eb4c682afefa48d76f20334f
Norman Kämper-Leymann <norman.leymann@gmail.com>
2017-03-28 21:47:21 +0200

    Migrate validation

--- src/Form/CSVimportForm.php
+++ src/Form/CSVimportForm.php
@@ -8,7 +8,7 @@ namespace Drupal\csvimport\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Component\Utility\Bytes;
+use Drupal\file\Entity\File;
 
 /**
  * Implements the ajax demo form controller.
@@ -37,28 +37,83 @@ class CSVimportForm extends FormBase {
     $form['#attributes'] = [
       'enctype' => 'multipart/form-data',
     ];
-    $form['csvfile']     = [
-      '#title'       => t('CSV File'),
-      '#type'        => 'file',
-      '#description' => ($max_size = file_upload_max_size()) ? t('Due to server restrictions, the <strong>maximum upload file size is @max_size</strong>. Files that exceed this size will be disregarded.', ['@max_size' => format_size($max_size)]) : '',
+
+    $form['csvfile'] = [
+      '#title'            => t('CSV File'),
+      '#type'             => 'file',
+      '#description'      => ($max_size = file_upload_max_size()) ? t('Due to server restrictions, the <strong>maximum upload file size is @max_size</strong>. Files that exceed this size will be disregarded.', ['@max_size' => format_size($max_size)]) : '',
+      '#element_validate' => ['::csvimport_validate_fileupload'],
     ];
-    $form['submit']      = [
+
+    $form['submit'] = [
       '#type'  => 'submit',
-      '#value' => t('Commence Import'),
+      '#value' => t('Start Import'),
     ];
-    $form['#validate']   = [
-      'csvimport_validate_fileupload',
-      'csvimport_form_validate',
-    ];
-    return $form;
+
+    // $form['#validate'] = [
+    //   'csvimport_validate_fileupload',
+    //   'csvimport_form_validate',
+    // ];
 
     return $form;
+  }
+
+  /**
+   * Validate the file upload. It must be a CSV, and we must
+   * successfully save it to our import directory.
+   */
+  public static function csvimport_validate_fileupload(&$element, FormStateInterface $form_state, &$complete_form) {
+
+    $validators = [
+      'file_validate_extensions' => ['csv CSV'],
+    ];
+
+    if ($file = file_save_upload('csvfile', $validators, FALSE, 0, FILE_EXISTS_REPLACE)) {
+      // The file was saved using file_save_upload() and was added to
+      // the files table as a temporary file. We'll make a copy and let
+      // the garbage collector delete the original upload.
+      $csv_dir          = 'temporary://csvfile';
+      $directory_exists = file_prepare_directory($csv_dir, FILE_CREATE_DIRECTORY);
+
+      if ($directory_exists) {
+        $destination = $csv_dir . '/' . $file->getFilename();
+        if (file_copy($file, $destination, FILE_EXISTS_REPLACE)) {
+          $form_state->setValue('csvupload', $destination);
+        }
+        else {
+          $form_state->setErrorByName('csvimport', t('Unable to copy upload file to @dest', ['@dest' => $destination]));
+        }
+      }
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+
+    if ($csvupload = $form_state->getValue('csvupload')) {
+      if ($handle = fopen($csvupload, 'r')) {
+        if ($line = fgetcsv($handle, 4096)) {
+          /**
+           * Validate the uploaded CSV here.
+           *
+           * The example CSV happens to have cell A1 ($line[0]) as
+           * below; we validate it only.
+           *
+           * You'll probably want to check several headers, eg:
+           *   if ( $line[0] == 'Index' || $line[1] != 'Supplier' || $line[2] != 'Title' )
+           */
+          if ($line[0] != 'Example CSV for csvimport.module - http://github.com/xurizaemon/csvimport') {
+            $form_state->setErrorByName('csvfile', t('Sorry, this file does not match the expected format.'));
+          }
+        }
+        fclose($handle);
+      }
+      else {
+        $form_state->setErrorByName('csvfile', t('Unable to read uploaded file @filepath', ['@filepath' => $csvupload]));
+      }
+    }
   }
 
   /**
